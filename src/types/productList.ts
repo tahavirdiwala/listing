fad88635209cbc6441459b8ae566504dc76b0b66
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-12-09 10:08:29 +0530

    .

--- src/types/productList.ts
+++ src/types/productList.ts
@@ -1,12 +1,12 @@
 type TProductList = {
-    body: {
-        toShow: Array<{
-            name: string,
-            brandName: string
-        }>;
-        currentPage: number;
-        totalPages: number;
-    };
+  body: {
+    toShow: Array<{
+      name: string;
+      brandName: string;
+    }>;
+    currentPage: number;
+    totalPages: number;
+  };
 };
 
-export { type TProductList }
+export { type TProductList };
